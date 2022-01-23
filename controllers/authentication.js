8a5f4d194978b0ceb48f97b092e2a25a71524841
NightmanCZ90 <novak.jan90@gmail.com>
2022-01-23 01:14:54 +0100

    feat: Add signup validation and hash password

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,16 +1,23 @@
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
 
 exports.signup = async (req, res, next) => {
   const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'You must provide email and password' });
+  }
 
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(422).send({ error: 'Email is in use' });
     }
+
+    const hashedPw = await bcrypt.hash(password, 12);
     const user = new User({
       email,
-      password,
+      password: hashedPw,
     });
     await user.save();
 
